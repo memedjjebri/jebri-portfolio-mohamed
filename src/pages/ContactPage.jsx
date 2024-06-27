9e6696fbeaec8be5153fe9d376e46ab3be400059
Mohamed Jebri <159800102+memedjjebri@users.noreply.github.com>
2024-06-27 11:05:52 +0200

    fix contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,36 @@
-
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const ContactPage = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_qnb6qeq",
+        "template_42amrxr",
+        form.current,
+        "JKa8HxkZy5OcvSH5X"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Message sent successfully!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("An error occurred, please try again.");
+        }
+      );
+  };
+
   return (
     <div className="mb-24 max-w-md mx-auto relative overflow-hidden z-10 bg-gray-800 p-8 rounded-lg shadow-md mt-8 before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
       <h2 className="text-2xl font-bold text-white mb-6">Get in touch</h2>
 
-      <form method="post" action="#">
+      <form ref={form} onSubmit={sendEmail}>
         <div className="mb-4">
           <label
             className="block text-sm font-medium text-gray-300"
@@ -18,6 +43,7 @@ const ContactPage = () => {
             type="text"
             id="name"
             name="name"
+            required
           />
         </div>
 
@@ -33,17 +59,23 @@ const ContactPage = () => {
             type="email"
             id="email"
             name="email"
+            required
           />
         </div>
 
         <div className="mb-4">
           <label
             className="block text-sm font-medium text-gray-300"
-            htmlFor="bio"
+            htmlFor="message"
           >
             Message
           </label>
-          <textarea className=" mt-2 p-16 w-full bg-gray-700 border border-gray-600 rounded-md text-white"></textarea>
+          <textarea
+            className="mt-2 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white"
+            id="message"
+            name="message"
+            required
+          ></textarea>
         </div>
 
         <div className="flex justify-end">
