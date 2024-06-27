5e54684bb451c48c0cf29503b174b2b758e447f7
Mohamed Jebri <159800102+memedjjebri@users.noreply.github.com>
2024-06-27 11:25:26 +0200

    fix contact page/footer

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -28,7 +28,7 @@ const ContactPage = () => {
 
   return (
     <div className="mb-24 max-w-md mx-auto relative overflow-hidden z-10 bg-gray-800 p-8 rounded-lg shadow-md mt-8 before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
-      <h2 className="text-2xl font-bold text-white mb-6">Get in touch</h2>
+      <h2 className="text-2xl font-bold text-white mb-48">Get in touch</h2>
 
       <form ref={form} onSubmit={sendEmail}>
         <div className="mb-4">
@@ -36,7 +36,22 @@ const ContactPage = () => {
             className="block text-sm font-medium text-gray-300"
             htmlFor="name"
           >
-            Full Name
+            First Name
+          </label>
+          <input
+            className="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white"
+            type="text"
+            id="name"
+            name="name"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-sm font-medium text-gray-300"
+            htmlFor="name"
+          >
+            Last Name
           </label>
           <input
             className="mt-1 p-2 w-full bg-gray-700 border border-gray-600 rounded-md text-white"
