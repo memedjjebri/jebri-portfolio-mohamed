2baa1d98f0dab82ab82aee080084d5191528d040
Mohamed Jebri <159800102+memedjjebri@users.noreply.github.com>
2024-06-27 10:25:21 +0200

    fix tailwind css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import logo from "../assets/logo.png";
-
+  import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -12,13 +12,11 @@ const Navbar = () => {
     <nav className="mx-auto relative overflow-hidden z-10 bg-gray-800 p-8 rounded-lg shadow-md  before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
       <div className="flex justify-between items-center">
         <img src={logo} alt="Logo" className="w-24 h-24 rounded-md" />
-
         <div className="mx-7">
           <h4 className="text-4xl text-w text-white uppercase font-bold">
             J<span className="text-indigo-600">EB</span>RI
           </h4>
         </div>
-
         <div className="md:hidden">
           <button
             onClick={toggleMenu}
@@ -40,21 +38,22 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
+      
         <ul className="hidden md:flex space-x-4">
           <li>
-            <a href="/" className="text-white hover:text-gray-400">
+            <Link to="/" className="text-white hover:text-gray-400">
               Accueil
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/projects" className="text-white hover:text-gray-400">
+            <Link to="/projects" className="text-white hover:text-gray-400">
               Projets
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="/contact" className="text-white hover:text-gray-400">
+            <Link to="/contact" className="text-white hover:text-gray-400">
               Contact
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
